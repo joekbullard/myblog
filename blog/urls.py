d790d06bbc5ded8b533d4b43c05a630d6f0d209e
Joe Bullard <joekbullard@gmail.com>
2021-12-20 01:13:23 +0000

    about page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexPageView
+from .views import IndexPageView, AboutPageView
 
 urlpatterns = [
-    path('', IndexPageView.as_view(), name='index')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', IndexPageView.as_view(), name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexPageView(TemplateView):
     template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
