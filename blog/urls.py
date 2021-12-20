fa4096a1d3e2b8104717c7f90de7adf019bafe89
Joe Bullard <joekbullard@gmail.com>
2021-12-20 10:31:19 +0000

    implement detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexPageView, AboutPageView
+from .views import IndexPageView, AboutPageView, BlogDetailView
 
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('', IndexPageView.as_view(), name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Post
 
 class IndexPageView(ListView):
     model = Post
     template_name = 'index.html'
     context_object_name = 'all_posts_list'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
