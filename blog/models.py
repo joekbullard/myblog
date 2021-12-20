234f5aa13e9188459fd9a0dfa3bcda2fda0a8b64
Joe Bullard <joekbullard@gmail.com>
2021-12-20 11:04:36 +0000

    slug urls.py implementation

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    slug = models.SlugField(null=True, unique=True, max_length=250)
+    slug = models.SlugField(null=False, unique=True, max_length=250)
     post_image = models.ImageField(default='default.png', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import IndexPageView, AboutPageView, BlogDetailView
 
 urlpatterns = [
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('<slug:slug>', BlogDetailView.as_view(), name='post_detail'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('', IndexPageView.as_view(), name='index'),
 ]
