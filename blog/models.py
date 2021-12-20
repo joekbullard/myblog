7b9ea100abeb8813928f6d0247c598d10e16ee0d
Joe Bullard <joekbullard@gmail.com>
2021-12-20 01:48:09 +0000

    base model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
+# blog/models.py
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    post_image = models.ImageField(default='default.png', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    body = models.TextField
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created']
+    
+    
+    
