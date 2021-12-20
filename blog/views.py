524ead7e7de8d0237008c3ee6f31b28f2a111342
Joe Bullard <joekbullard@gmail.com>
2021-12-20 00:59:13 +0000

    index.html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    pass
+class HomePageView(TemplateView):
+    template_name = 'index.html'
