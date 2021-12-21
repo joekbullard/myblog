1b7b0d453a0de1b4d71e9e902aace5a1a5685898
Joe Bullard <joekbullard@gmail.com>
2021-12-21 21:29:49 +0000

    removed pre-populated field

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,6 @@ from .models import Post
 from markdownx.admin import MarkdownxModelAdmin
 
 class PostAdmin(MarkdownxModelAdmin):
-    list_display = ('title', 'created',)
-    prepopulated_fields = {'slug': ('title',)} # new
+    list_display = ('title', 'created',) # new
 
 admin.site.register(Post, PostAdmin)
